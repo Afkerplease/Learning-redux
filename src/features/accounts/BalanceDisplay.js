853a1cc8aa1d739e0ea0482c742b95692abca07f
afker please <afker2020@gmail.com>
2024-01-30 02:21:42 +0100

    last commit

---
+++ src/features/accounts/BalanceDisplay.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import store from "../../store";
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function BalanceDisplay() {
+  const balance = useSelector((store) => store.account.balance);
+  return <div className="balance">{balance}$</div>;
+}
+
+export default BalanceDisplay;
